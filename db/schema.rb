18ebfabee3dd5f0b0b1ae4402328223cf3d4367a
lesliesage <leslie.sage@gmail.com>
2020-05-27 08:47:08 -0400

    added email to user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_27_005727) do
+ActiveRecord::Schema.define(version: 2020_05_27_123644) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 2020_05_27_005727) do
     t.string "password_digest"
     t.string "reset_password_code"
     t.datetime "reset_password_sent_at"
+    t.string "email"
   end
 
 end
